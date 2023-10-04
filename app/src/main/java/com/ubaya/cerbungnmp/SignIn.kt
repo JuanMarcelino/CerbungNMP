d2d4ad099b92afe26e121749fcf3cffab4af5bf4
JuanMarcelino <146976610+JuanMarcelino@users.noreply.github.com>
2023-10-04 23:45:34 +0700

    Project NMP

---
+++ app/src/main/java/com/ubaya/cerbungnmp/SignIn.kt
@@ -0,0 +1,13 @@
+package com.ubaya.cerbungnmp
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn : AppCompatActivity() {
+    //private lateinit var binding:ActivitySignInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
